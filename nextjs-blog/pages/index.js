13ab16734dc0f47e5e497558abee59aa51e92913
hira <ohno@toreta.in>
2020-04-29 02:21:55 +0900

    Add link to blog posts

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,5 +1,7 @@
 import Head from "next/head";
+import Link from "next/link";
 import Layout, { siteTitle } from "../components/layout";
+import Date from "../components/date";
 import utilStyles from "../styles/utils.module.css";
 import { getSortedPostData } from "../lib/posts";
 
@@ -30,11 +32,13 @@ const Home = ({ allPostsData }) => {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href="/posts/[id]" as={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
